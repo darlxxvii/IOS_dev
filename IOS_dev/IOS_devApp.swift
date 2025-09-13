fa49f70f0f6780062477b926faa16954e6f607e4
darlxxvii <jekenn9@gmail.com>
2025-09-14 00:20:41 +0500

    Initial Commit

---
+++ IOS_dev/IOS_devApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_devApp.swift
+//  IOS_dev
+//
+//  Created by Nazerke Zheken on 14.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct IOS_devApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
