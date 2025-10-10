e49c7a004c0ce91589c5658bc6a88bc84f54b61e
Nazerke <157003668+darlxxvii@users.noreply.github.com>
2025-10-10 23:21:30 +0500

    Create Address.swift

---
+++ lab3/ShoppingCartAssignment/Sources/Address.swift
@@ -0,0 +1,12 @@
+//part 3.1
+
+struct Address {
+    let street: String
+    let city: String
+    let zipCode: String
+    let country: String
+    
+    var formattedAddress: String {
+        "\(street)\n\(city), \(zipCode)\n\(country)"
+    }
+}
