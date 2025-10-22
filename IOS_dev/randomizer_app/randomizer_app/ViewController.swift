1bf28844ccfa1b4e304a34e500ff70134b7bb613
darlxxvii <jekenn9@gmail.com>
2025-10-22 22:31:13 +0500

    Finished lab5

---
+++ IOS_dev/randomizer_app/randomizer_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  randomizer_app
+//
+//  Created by Nazerke Zheken on 22.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ IOS_dev/randomizer_app/randomizer_appTests/randomizer_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  randomizer_appTests.swift
+//  randomizer_appTests
+//
+//  Created by Nazerke Zheken on 22.10.2025.
+//
+
+import Testing
+@testable import randomizer_app
+
+struct randomizer_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
