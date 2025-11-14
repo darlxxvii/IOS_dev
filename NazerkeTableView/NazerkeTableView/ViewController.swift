52f6d184d239d01fd2455429b5ec4cb01cc4ded4
darlxxvii <jekenn9@gmail.com>
2025-11-14 10:15:44 +0500

    Initial commit

---
+++ NazerkeTableView/NazerkeTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NazerkeTableView
+//
+//  Created by Nazerke Zheken on 14.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NazerkeTableView/NazerkeTableViewTests/NazerkeTableViewTests.swift
@@ -0,0 +1,17 @@
+//
+//  NazerkeTableViewTests.swift
+//  NazerkeTableViewTests
+//
+//  Created by Nazerke Zheken on 14.11.2025.
+//
+
+import Testing
+@testable import NazerkeTableView
+
+struct NazerkeTableViewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
